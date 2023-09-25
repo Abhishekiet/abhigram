6f75a60800bb36275379970a795ed4a042185f18
Abhishekiet <62215477+Abhishekiet@users.noreply.github.com>
2023-09-25 18:56:23 +0530

    Update Header.js

--- app/components/Header.js
+++ app/components/Header.js
@@ -11,7 +11,7 @@ const Header=()=>{
     const session = useSession()
     
     return(
-        <div className="flex justify-between p-4 bg-[black] sticky items-center">
+        <div className="flex justify-between p-4 h-[10vh] bg-[black] sticky items-center">
             <Link href="/addPost"> <MdAddCircleOutline size={35} className="text-white" /> </Link>
             <Link href='/'> <h1 className="text-3xl text-white font-bold" style={{ fontFamily:'cursive' }} > AbhiGram </h1> </Link>
             <Link href={ `/userPage` }> <Image src={ session.status === 'unauthenticated'? '/google.png':session?.data?.user?.image } className="rounded-full" alt='' height={50} width={50} /> </Link>
