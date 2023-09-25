abff5d37d6799f15e3fa20ba97fd7fe07f28055c
Abhishekiet <62215477+Abhishekiet@users.noreply.github.com>
2023-09-25 19:00:28 +0530

    Update Footer.js

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -1,6 +1,6 @@
 const Footer=()=>{
     return(
-        <div className=" p-2 flex items-center bg-[black] absolute bottom-0 w-full h-[5vh] justify-center">
+        <div className=" p-2 flex items-center bg-[black] w-full h-[5vh] justify-center">
             <h1 className=" text-white text-xl font-bold" style={{ fontFamily:'cursive' }}> Developed by Abhishek Dameria </h1>
         </div>
     )
